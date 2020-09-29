819d1d4184d6140ceedac6332eb60bacdf8210dd
madridwan <madridwan94@gmail.com>
2020-09-29 10:18:08 +0700

    link ke app

---
+++ index.php
@@ -0,0 +1,34 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="icon" href="assets/img/favicons/apple-touch-icon.png" type="image/x-icon">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+
+    <title>BRImo</title>
+  </head>
+  <body>
+	<!-- <section class="jumbotron text-center"> -->
+		<div class="container flex-grow-1 py-4">
+			<div class="text-center py-3 my-3">
+				<img src="assets/img/selamat_selamat_copy.jpg" class="img-fluid text-center">
+			</div>
+			<div class="text-center py-3 my-3">
+				<h3 class="text-primary font-weight-bold">Selamat! Akun BRImo kamu sudah siap</h3>
+				<p class="lead text-muted">Silahkan masuk ke aplikasi BRIMO, untuk mengaktifkan akunmu,dan nikmati kemudahan bertransaksi digital dengan BRIMO</p>
+			</div>
+			<div class="text-center py-3 my-3">
+        <a class="btn btn-primary btn-lg btn-block" href="javascript:void(0)" onclick="location.href='intent://#Intent;package=id.co.bri.brimo;scheme=https;end'" role="button">Buka BRImo</a>
+        <a class="btn btn-primary btn-lg btn-block" href="javascript:void(0)" onclick="location.href='intent://#Intent;package=com.skype.raider;scheme=skype;end'" role="button">Skype</a>
+        <a class="btn btn-primary btn-lg btn-block" href="javascript:void(0)" onclick="location.href='intent://www.youtube.com/#Intent;package=com.google.android.youtube;scheme=https;end'" role="button">youtube</a>
+			</div>
+		</div>
+	<!-- </section> -->
+  </body>
+</html>
