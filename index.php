db10c215e6a6bdbcd9244cc37310727c3c8a681d
madridwan <madridwan94@gmail.com>
2020-09-29 17:22:10 +0700

    mengubah intent

--- index.php
+++ index.php
@@ -24,9 +24,7 @@
 				<p class="lead text-muted">Silahkan masuk ke aplikasi BRIMO, untuk mengaktifkan akunmu,dan nikmati kemudahan bertransaksi digital dengan BRIMO</p>
 			</div>
 			<div class="text-center py-3 my-3">
-        <a class="btn btn-primary btn-lg btn-block" href="javascript:void(0)" onclick="location.href='intent://#Intent;package=id.co.bri.brimo;scheme=https;end'" role="button">Buka BRImo</a>
-        <a class="btn btn-primary btn-lg btn-block" href="javascript:void(0)" onclick="location.href='intent://#Intent;package=com.skype.raider;scheme=skype;end'" role="button">Skype</a>
-        <a class="btn btn-primary btn-lg btn-block" href="javascript:void(0)" onclick="location.href='intent://www.youtube.com/#Intent;package=com.google.android.youtube;scheme=https;end'" role="button">youtube</a>
+        <a class="btn btn-primary btn-lg btn-block" href="javascript:void(0)" onclick="location.href='intent://#Intent;package=com.project.bri.brimo;scheme=https;end'" role="button">Buka BRImo</a>
 			</div>
 		</div>
 	<!-- </section> -->
